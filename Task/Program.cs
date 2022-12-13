572167f3ff8e58a127041f3c73e17a052664d9b8
kkormakova <109741103+kkormakova@users.noreply.github.com>
2022-12-13 17:01:59 +0700

    add all code in task

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,5 +1,9 @@
-﻿string[] array1 = new string[5] { "09744", "6774", "hello", "123", "arc" };
+﻿string[] array1 = new string[5] { "097", "6774", "hello", "123", "arc" };
 string[] array2 = new string[array1.Length];
+CheckArray(array1, array2);
+PrintArray(array2);
+
+
 void CheckArray(string[] arr1, string[] arr2)
 {
     int count = 0;
@@ -11,4 +15,12 @@ void CheckArray(string[] arr1, string[] arr2)
             count++;
         }
     }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
 }
