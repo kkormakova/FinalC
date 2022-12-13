dbffb117e4176b2ec79bd28ca1698cd755bdcd19
kkormakova <109741103+kkormakova@users.noreply.github.com>
2022-12-13 16:58:05 +0700

    add all folders and README

---
+++ Task/Program.cs
@@ -0,0 +1,14 @@
+﻿string[] array1 = new string[5] { "09744", "6774", "hello", "123", "arc" };
+string[] array2 = new string[array1.Length];
+void CheckArray(string[] arr1, string[] arr2)
+{
+    int count = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+        if (arr1[i].Length <= 3)
+        {
+            arr2[count] = arr1[i];
+            count++;
+        }
+    }
+}
